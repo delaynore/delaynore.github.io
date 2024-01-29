d32e0ec9cc4d7f44283056465d691275515ab4b0
DanyaLozhkin <delaynore@gmail.com>
2024-01-29 16:08:30 +0400

    Добавлен react

--- script.js
+++ script.js
@@ -1,86 +1,131 @@
-const data = { "sources": [{ "id": 1, "name": "meduza" }, { "id": 6, "name": "3dnews" }, { "id": 7, "name": "nytimes" }, { "id": 8, "name": "forbes" }, { "id": 9, "name": "igromania" }, { "id": 10, "name": "buro237" }, { "id": 11, "name": "rusvesna" }, { "id": 12, "name": "7ya" }, { "id": 14, "name": "aif" }, { "id": 15, "name": "gazetaru" }, { "id": 16, "name": "karpov.courses" }], "categories": [{ "id": 1, "name": "tech" }, { "id": 2, "name": "sport" }, { "id": 3, "name": "fashion" }, { "id": 4, "name": "politics" }, { "id": 5, "name": "other" }, { "id": 6, "name": "karpov.courses" }], "items": [{ "id": 493350, "lang": "ru", "date": "Thu Jan 25 2024 10:31:59 GMT+0300 (Moscow Standard Time)", "title": "Самолет с главой МИД ФРГ Бербок не пустили в Эритрею ", "description": "Самолет с министром иностранных дел ФРГ Анналеной Бербок во время полета в Джибути совершил незапланированную посадку в Саудовской Аравии, поскольку не получил разрешения на пролет над территорией Эритреи. Об этом пишет ZDF.\n\nПеред ...", "image": "https://img.gazeta.ru/files3/575/16172575/2023-01-19T125556Z_931552033_RC2P1X97LM69_RTRMADP_3_GERMANY-YAZIDIS-GENOCIDE-pic905-895x505-35161.jpg", "source_id": 15, "category_id": 4 }, { "id": 493327, "lang": "ru", "date": "Thu Jan 25 2024 10:31:47 GMT+0300 (Moscow Standard Time)", "title": "Надзорные органы КНР наказали в 2023 году более 600 тыс. чиновников", "description": "Контролирующим ведомствам поступило более 3,5 миллиона обращений.", "image": "https://aif-s3.aif.ru/images/035/604/866dd2bf0eeb0af671cfd8e46744e84f.jpg", "source_id": 14, "category_id": 4 }, { "id": 493332, "lang": "ru", "date": "Thu Jan 25 2024 10:31:18 GMT+0300 (Moscow Standard Time)", "title": "В суд поступил иск о взыскании более 358 млрд рублей с «крабового короля» Олега Кана", "description": "Генпрокуратура подала иск о взыскании 358 млрд рублей с группы ответчиков, в числе которых «крабовый король» Олег Кан. Прошлым летом прокуроры передали в суд дело против бизнесмена об организации преступного сообщества, контрабанде, уклонении от уплаты таможенных платежей и налогов. Кроме того, Кана обвиняют в организации убийства предпринимателя в 2010 году", "image": "", "source_id": 8, "category_id": 5 }, { "id": 493333, "lang": "ru", "date": "Thu Jan 25 2024 10:31:10 GMT+0300 (Moscow Standard Time)", "title": "В Оренбуржье три человека пострадали при обрушении насосной станции", "description": "", "image": "", "source_id": 14, "category_id": 5 }, { "id": 493375, "lang": "ru", "date": "Thu Jan 25 2024 10:30:25 GMT+0300 (Moscow Standard Time)", "title": "В Сбере назвали причины оформления студентами вкладов и накопительных счетов", "description": "Большинство (60%) российских студентов заявили о том, что оформляют вклады, накопительные счета и накопительное страхование жизни для формирования накоплений и обретения финансовой независимости. Об этом свидетельствуют данные, полученные ...", "image": "https://img.gazeta.ru/files3/412/17479412/2023-08-05T050013Z_545467048_RC2Q41AYNKOV_RTRMADP_3_CHINA-RUSSIA-DIPLOMACY-pic905-895x505-76520.jpg", "source_id": 15, "category_id": 4 }, { "id": 493388, "lang": "ru", "date": "Thu Jan 25 2024 10:30:23 GMT+0300 (Moscow Standard Time)", "title": "Специалист по гипоксии Ветош назвал азотную казнь самой гуманной", "description": "Казнь азотом, скорее всего, не приводит к мучениям человека: он чувствует эйфорию, а затем теряет сознание. Об этом \"Газете.Ru\" рассказал профессор, доктор биологических наук, сотрудник Национального госуниверситета физической ...", "image": "https://img.gazeta.ru/files3/195/18178195/shutterstock_618892196-pic905-895x505-22365.jpg", "source_id": 15, "category_id": 1 }, { "id": 493336, "lang": "ru", "date": "Thu Jan 25 2024 10:30:20 GMT+0300 (Moscow Standard Time)", "title": "В Белоруссии 20 человек обвинили в заговоре с целью захвата власти", "description": "Начат процесс специального производства против 20 так называемых аналитиков Тихановской.", "image": "https://aif-s3.aif.ru/images/035/604/6ec5b7cab153801ae9ed9d4e86da1c89.jpg", "source_id": 14, "category_id": 5 }, { "id": 493384, "lang": "ru", "date": "Thu Jan 25 2024 10:29:41 GMT+0300 (Moscow Standard Time)", "title": "Модель Джиджи Хадид заметили с актером Брэдли Купером", "description": "Модель Джиджи Хадид заметили вместе с голливудским актером Брэдли Купером на фоне слухов о романе. Фотографии были опубликованы на сайте Page Six. \n\nПапарацци удалось заснять знаменитостей 23 января в аэропорту Нью-Йорка. ...", "image": "http://img.gazeta.ru/files3/783/17710783/435653-pic905-895x505-47297.jpg", "source_id": 15, "category_id": 3 }, { "id": 493341, "lang": "ru", "date": "Thu Jan 25 2024 10:27:57 GMT+0300 (Moscow Standard Time)", "title": "В Карелии четыре человека погибли в ДТП на трассе «Кола»", "description": "Еще трех человек, в том числе двух детей, доставили в больницу.", "image": "https://aif-s3.aif.ru/images/035/604/5e7ced1bcbdd236505fd7d939d437d0f.jpg", "source_id": 14, "category_id": 5 }, { "id": 493345, "lang": "ru", "date": "Thu Jan 25 2024 10:27:42 GMT+0300 (Moscow Standard Time)", "title": "Mash: актер из «Папиных дочек» Леонов сломал палец на репетиции в «Ленкоме»", "description": "Артист повредил палец дверью.", "image": "https://aif-s3.aif.ru/images/035/604/1d9c99616f50aa4f3a5c526302c66ea2.jpg", "source_id": 14, "category_id": 5 }, { "id": 493376, "lang": "ru", "date": "Thu Jan 25 2024 10:23:53 GMT+0300 (Moscow Standard Time)", "title": "Стало известно, во сколько России обойдется импортозамещение добычи нефти и газа", "description": "На разработку российских аналогов оборудования для бурения скважин и нефтегазовой добычи на суше будет направлено 19,8 млрд рублей. Об этом пишет газета \"Ведомости\" со ссылкой на паспорт  дорожной карты, разработанный экспертами  ...", "image": "https://img.gazeta.ru/files3/183/17869183/AP18341683465367-pic905-895x505-35057.jpg", "source_id": 15, "category_id": 4 }, { "id": 493348, "lang": "ru", "date": "Thu Jan 25 2024 10:19:55 GMT+0300 (Moscow Standard Time)", "title": "На руднике «Николаевский» в Приморье из-за пожара эвакуировали 68 горняков", "description": "Задымление обнаружили на горизонте 275 метров.", "image": "https://aif-s3.aif.ru/images/035/604/ee6493336d4d1d63c63f237cf5afc9f2.jpeg", "source_id": 14, "category_id": 5 }, { "id": 493353, "lang": "ru", "date": "Thu Jan 25 2024 10:17:27 GMT+0300 (Moscow Standard Time)", "title": "В Эстонии заявили, что НАТО недооценило военные возможности России", "description": "Командующий Силами обороны Эстонии генерал Мартин Херем заявил, что прогнозы стран НАТО об истощении российской армии не оправдались, Запад недооценил \"вооруженные силы президента Владимира Путина\". Его слова приводит ...", "image": "https://img.gazeta.ru/files3/227/18137227/Martin_Herem__2021__extract-pic905-895x505-57957.jpg", "source_id": 15, "category_id": 4 }, { "id": 493377, "lang": "ru", "date": "Thu Jan 25 2024 10:15:51 GMT+0300 (Moscow Standard Time)", "title": "В Сбере раскрыли эффект от изменения условий по льготной ипотеке", "description": "Пересмотр условий по льготной ипотеке отрезал от кредитов с низкими ставками инвесторов, а не обычных покупателей жилья. Об этом заявил первый зампред \"Сбера\" Кирилл Царев, сообщает РБК. \n\nЦарев объяснил, что в программа 2020 ...", "image": "https://img.gazeta.ru/files3/862/17542862/RIA_3291716-pic905-895x505-50501.jpg", "source_id": 15, "category_id": 4 }, { "id": 493335, "lang": "ru", "date": "Thu Jan 25 2024 10:13:27 GMT+0300 (Moscow Standard Time)", "title": "Гендиректор «Диасофта» заявил о планах провести IPO в первой половине 2024 года", "description": "Разработчик ПО для финансового сектора «Диасофт» рассматривает возможность проведения IPO, сказал гендиректор компании Александр Глазков. По его словам, выйти на биржу компания может в первом полугодии 2024 года", "image": "", "source_id": 8, "category_id": 5 }, { "id": 493352, "lang": "ru", "date": "Thu Jan 25 2024 10:13:26 GMT+0300 (Moscow Standard Time)", "title": "В РФ призвали ужесточить ответственность за организацию приютов в квартирах", "description": "В пунктах передержки нередко нарушаются санитарные нормы.", "image": "https://aif-s3.aif.ru/images/035/604/b6848b948b93592cd6364126cda1067b.jpg", "source_id": 14, "category_id": 5 }, { "id": 493330, "lang": "ru", "date": "Thu Jan 25 2024 10:09:43 GMT+0300 (Moscow Standard Time)", "title": "Исполнитель роли Росомахи Хью Джекман завершил съёмки в «Дэдпуле 3» свежими фото", "description": "", "image": "https://newcdn.igromania.ru/articles/pics/tmp/images/2024/1/25/a4d3b189-d7c4-4e1f-af75-6be542ab9183.jpg", "source_id": 9, "category_id": 1 }, { "id": 493354, "lang": "ru", "date": "Thu Jan 25 2024 10:08:14 GMT+0300 (Moscow Standard Time)", "title": "В Белгороде звучит сирена ракетной опасности", "description": "Жителей призвали оставаться дома и не подходить к окнам.", "image": "https://aif-s3.aif.ru/images/035/604/8e5b3f2ed3a28c30b591e005b419e9d1.jpg", "source_id": 14, "category_id": 5 }, { "id": 493385, "lang": "ru", "date": "Thu Jan 25 2024 10:07:13 GMT+0300 (Moscow Standard Time)", "title": "Стилист Скороходова: объемные костюмы будут в моде весной-летом 2024 года", "description": "Платья в стиле невесты, одежда с открытыми плечами, объемные костюмы и геометричные формы в образе будут в тренде весной-летом 2024 года. Об этом \"Москве 24\" рассказала стилист Анна Скороходова.\n\nПо ее словам, в моде в этом ...", "image": "http://img.gazeta.ru/files3/492/14873492/pexels-ron-lach-8387811-pic905-895x505-13298.jpg", "source_id": 15, "category_id": 3 }, { "id": 493355, "lang": "ru", "date": "Thu Jan 25 2024 10:06:47 GMT+0300 (Moscow Standard Time)", "title": "Следком Белоруссии обвинил 20 человек в заговоре", "description": "Следственный комитет (СК) Белоруссии начал заочное производство в отношении 20 человек, которых обвиняют в участии в заговоре с целью захвата власти. Об этом ведомство сообщило в Telegram-канале.\n\nВ ведомстве отметили, что следователи ...", "image": "https://img.gazeta.ru/files3/904/13660904/20111107_zaf_x99_196-pic905-895x505-17791.jpg", "source_id": 15, "category_id": 4 }] }
 
-const categories = new Map(data.categories.map(item => [item.id, item.name]));
-const sources = new Map(data.sources.map(item => [item.id, item.name]));
-
-const mainNews = data.items.slice(0, 3);
-const smallNews = data.items.slice(3, 12);
-
-const mainNewsContainer = document.querySelector('.articles__big-column');
-const smallNewsContainer = document.querySelector('.articles__small-column');
-
-const createMainNewsItem = (item) => {
-  const article = document.createElement('article');
-  article.classList.add('main-article');
-
-  const divImage = document.createElement('div');
-  divImage.classList.add('main-article__image-container');
-  article.appendChild(divImage);
-
-  const image = document.createElement('img');
-  image.src = item.image === "" ? 'https://placehold.co/600x400?text=Not\\nFound' : item.image;
-  image.classList.add('main-article__image');
-  image.alt = 'image';
-
-  divImage.appendChild(image);
-
-  const divContent = document.createElement('div');
-  divContent.classList.add('main-article__content');
-  article.appendChild(divContent);
-  
-  const spanCategory = document.createElement('span');
-  spanCategory.classList.add('article-category', 'main-article__category');
-  spanCategory.textContent = categories.get(item.category_id);
-  divContent.appendChild(spanCategory);
-
-  const h2Title = document.createElement('h2');
-  h2Title.classList.add('main-article__title');
-  h2Title.textContent = item.title;
-  divContent.appendChild(h2Title);
-
-  const text = document.createElement('p');
-  text.classList.add('main-article__text');
-  text.textContent = item.description;
-  divContent.appendChild(text);
-
-  const spanSource = document.createElement('span');
-  spanSource.classList.add('article-source', 'main-article__source');
-  spanSource.textContent = sources.get(item.source_id);
-  divContent.appendChild(spanSource);
-  
-  return article;
-};
-
-const createSmallNewsItem = (item) => {
-  const article = document.createElement('article');
-  article.classList.add('small-article');
-
-  const h2Title = document.createElement('h2');
-  h2Title.classList.add('small-article__title');
-  h2Title.textContent = item.title;
-  article.appendChild(h2Title);
-
-  const caption = document.createElement('p');
-  caption.classList.add('small-article__caption');
-  article.appendChild(caption);
-
-  const spanDate = document.createElement('span');
-  spanDate.classList.add('article-date', 'small-article__date');
-  spanDate.textContent = new Date(item.date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric' });
-  caption.appendChild(spanDate);
-
-  const spanSource = document.createElement('span');
-  spanSource.classList.add('article-source', 'small-article__source');
-  spanSource.textContent = sources.get(item.source_id);
-  caption.appendChild(spanSource);
-
-  return article;
+const category_id = {
+    index: 0,
+    fashion: 3,
+    technologies: 1,
+    sport: 2,
+    karpov: 6
 }
 
-mainNews.forEach(item => {
-  mainNewsContainer.appendChild(createMainNewsItem(item));
-});
+const category_names = {
+    index: 'Главная',
+    fashion: 'Мода',
+    technologies: 'Технологии',
+    sport: 'Спорт',
+    karpov: 'Karpov'
+}
 
-smallNews.forEach(item => {
-  smallNewsContainer.appendChild(createSmallNewsItem(item));
-});
+const App = () => {
+    const [category, setCategory] = React.useState('index');
+    const [articles, setArticles] = React.useState({ items: [], categories: [], sources: [] });
+    const onNavClick = (e) => {
+        e.preventDefault();
+        setCategory(e.currentTarget.dataset.href);
+    }
+
+    React.useEffect(() => {
+        fetch('http://frontend.karpovcourses.net/api/v2/ru/news/' + category_id[category] || '')
+            .then(response => response.json())
+            .then((response) => {
+                setArticles(response);
+            })
+    }, [category]);
+
+    return (
+        <>
+            <header className='header'>
+                <div className='container'>
+                    <nav className='navigation grid header__navigation'>
+                        <a href='./index.html' className='navigation__logo'>
+                            <img className='navigation__image' src='/images/logo.svg' alt='logo' />
+                        </a>
+                        <ul className='navigation__list'>
+                            {['index', 'fashion', 'technologies', 'sport', 'karpov'].map((item) => {
+                                return <li key={item} className='navigation__item'>
+                                    <a onClick={onNavClick}
+                                        data-href={item}
+                                        href='#'
+                                        className={`${(item === category ? 'navigation__link-active ' : '')}navigation__link`}
+                                    >
+                                        {category_names[item]}
+                                    </a>
+                                </li>
+                            })}
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <main className='main'>
+                <section className='articles'>
+                    <div className='container grid'>
+                        <section className='articles__big-column'>
+                            {articles.items.slice(0, 3).map((item) => {
+                                return (
+                                    <article className="main-article" key={item.id} >
+                                        <div className="main-article__image-container">
+                                            <img className="main-article__image" src={item.image || 'https://placehold.co/600x400?text=Not\\nFound'} alt="image" />
+                                        </div>
+                                        <div className="main-article__content">
+                                            <span className="article-category main-article__category">{articles.categories.find(e => e.id === item.category_id).name}</span>
+                                            <h2 className="main-article__title">{item.title}</h2>
+                                            <p className="main-article__text">
+                                                {item.description}
+                                            </p>
+                                            <span className="article-source main-article__source">{articles.sources.find(e => e.id === item.source_id).name}</span>
+                                        </div>
+                                    </article>
+                                )
+                            })}
+                        </section>
+                        <section className='articles__small-column'>
+                            {articles.items.slice(3, 12).map((item) => {
+                                return (
+                                    <article className="small-article" key={item.id}>
+                                        <h2 className="small-article__title">{item.title}</h2>
+                                        <p className="small-article__caption">
+                                            <span className="article-date small-article__date">{new Date(item.date).toLocaleDateString('ru-RU', { month: 'long', day: '2-digit' })}</span>
+                                            <span className="article-source small-article__source">{articles.sources.find((e) => e.id === item.source_id).name}</span>
+                                        </p>
+                                    </article>
+                                )
+                            })}
+                        </section>
+                    </div>
+                </section>
+            </main>
+            <footer className='footer'>
+                <div className='container'>
+                    <nav className='navigation grid footer__navigation'>
+                        <a href='./index.html' className='navigation__logo'>
+                            <img className='navigation__image' src='/images/logo.svg' alt='logo' />
+                        </a>
+                        <ul className='navigation__list'>
+                            {['index', 'fashion', 'technologies', 'sport', 'karpov'].map((item) => {
+                                return <li key={item} className='navigation__item'>
+                                    <a onClick={onNavClick}
+                                        data-href={item}
+                                        href='#'
+                                        className={`${(item === category ? 'navigation__link-active ' : '')}navigation__link`}
+                                    >
+                                        {category_names[item]}
+                                    </a>
+                                </li>
+                            })}
+                        </ul>
+                    </nav>
+                    <div className='footer__column container'>
+                        <p className='footer__text'>
+                            Сделано
+                            <a href='https://t.me/danidoub' target='_blank' className='footer__link'>
+                                Daniil Lozhkin
+                            </a>
+                        </p>
+                        <p className='footer__copyright'>© 2023</p>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
